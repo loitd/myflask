f6ccd41d246541e2204b4a27ff397fffaf8c9cc8
loitd <loitranduc@gmail.com>
2020-07-28 21:55:18 +0700

    lauto added and commited for minor fixes & features + deployment

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app1 import create_app
-  
-app = create_app()
-app.run(host="127.0.0.1", port=9001, debug=True)
+
+if __name__ == "__main__":
+        app = create_app()
+        app.run(debug=True)
