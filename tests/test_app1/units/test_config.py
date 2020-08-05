1e10c5083b381b298f16209dd2ab10adba45dd68
loitd <loitranduc@gmail.com>
2020-08-06 01:44:14 +0700

    lauto added and commited for minor fixes & features

--- tests/test_app1/units/test_config.py
+++ tests/test_app1/units/test_config.py
@@ -3,8 +3,8 @@ import pytest
 import os
 
 def test_env():
-    assert os.environ.get("FLASK_ENV") is not None
-    assert os.environ.get("FLASK_APP") == "app1"
+    # assert os.environ.get("FLASK_ENV") is not None
+    # assert os.environ.get("FLASK_APP") == "app1"
     assert os.environ.get("GH_CLIENT_KEY") is not None
     assert os.environ.get("GH_CLIENT_SECRET") is not None
     assert os.environ.get("GG_CLIENT_ID") is not None
